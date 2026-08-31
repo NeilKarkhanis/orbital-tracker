7e79044e3ce2146ae22c510d892cff66e453fa5e
Neil Karkhanis <neilkarkhanis@Neils-MacBook-Air-7.local>
2026-08-31 19:52:13 -0400

    Implementation of basic error handling

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import requests
+import os
+import json
 import plotly.graph_objects as go
 from skyfield.api import load, EarthSatellite
 from datetime import datetime, timedelta, UTC
@@ -11,28 +13,59 @@ st.set_page_config(
 )
 
 st.title("Orbital Collision Avoidance")
-st.write("V0.3 - Visualizing Satellite Orbit Around Earth (Search for Satellite through dropdown menu)")
+st.write("V0.4 - Visualizing Satellite Orbit Around Earth (Error handling)")
 
-def load_satellite():
-    url = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
-    response = requests.get(url, timeout=20)
+CACHE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "satellite_cache.json")
+CELESTRAK_URL = "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle"
+
+
+def fetch_catalog():
+    response = requests.get(CELESTRAK_URL, headers={"User-Agent": "Mozilla/5.0"}, timeout=15)
     response.raise_for_status()
 
     lines = [line.strip() for line in response.text.strip().splitlines() if line.strip()]
-
     catalog = {}
+    
     for i in range(0, len(lines) - 2, 3):
         name = lines[i]
-        line1 = lines[i+1]
-        line2 = lines[i+2]
-
+        line1 = lines[i + 1]
+        line2 = lines[i + 2]
         norad_id = line1[2:7].strip()
-        label = f"{name} ({norad_id})"
-        catalog[label] = (name, line1, line2)
+        catalog[f"{name} ({norad_id})"] = (name, line1, line2)
 
     return catalog
 
-catalog = load_satellite()
+
+refresh_select = st.sidebar.button("Refresh from Celestrak")
+download_req = refresh_select or not os.path.exists(CACHE_FILE)
+
+catalog = None
+if download_req:
+    try:
+        catalog = fetch_catalog()
+        with open(CACHE_FILE, "w") as f:
+            json.dump(catalog, f)
+
+    except requests.exceptions.Timeout:
+        st.warning("Too long to respond")
+
+    except requests.exceptions.ConnectionError:
+        st.warning("Couldn't connect")
+
+    except requests.exceptions.HTTPError as e:
+        st.warning(f"Error")
+
+if catalog is None:
+    if os.path.exists(CACHE_FILE):
+        st.info("Using last saved satellite")
+        with open(CACHE_FILE, "r") as f:
+            catalog = json.load(f)
+    else:
+        st.error("No satellite data available")
+        st.stop()
+
+catalog = {k: tuple(v) for k, v in catalog.items()}
+
 labels = sorted(catalog.keys())
 
 default_index = 0
@@ -43,8 +76,8 @@ for i, label in enumerate(labels):
 
 selected_label = st.selectbox(
     "Search for a satellite",
-    options = labels,
-    index = default_index
+    options=labels,
+    index=default_index
 )
 
 name, line, line2 = catalog[selected_label]
